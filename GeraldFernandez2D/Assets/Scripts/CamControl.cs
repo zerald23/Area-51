fd78562708d722304a0dfffb430f63d6f89b0bb5
zerald23 <zerald5@hotmail.com>
2018-10-02 13:04:04 -0500

    test 2d 3

--- GeraldFernandez2D/Assets/Scripts/CamControl.cs
+++ GeraldFernandez2D/Assets/Scripts/CamControl.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamControl : MonoBehaviour {
+public class CamControl : MonoBehaviour
+{
 
     public Transform target;
+    public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime)
-	}
 
-    Vector3 ConverToCanDepth (Vector3 vector)
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, ConvertToCamDepth(target.position), speed * Time.deltaTime);
+    }
+
+    Vector3 ConvertToCamDepth(Vector3 vector)
+    {
+        return new Vector3(vector.x, vector.y, Camera.main.transform.position.z);
+    }
 }
