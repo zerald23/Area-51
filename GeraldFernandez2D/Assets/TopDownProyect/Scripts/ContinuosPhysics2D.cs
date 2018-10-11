6a4cb8f4a5e09297d36d8bbb97c69debbc91ece0
zerald23 <zerald5@hotmail.com>
2018-10-11 13:15:06 -0500

    test 2d 6

---
+++ GeraldFernandez2D/Assets/TopDownProyect/Scripts/ContinuosPhysics2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuosPhysics2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 movement = Vector3.zero;
+        for (int i = 0; i < axes.Count; i++)
+        {
+            if (Input.GetKey(axes[i].keyCode) && !FindObstacle(axes[i].direction))
+            {
+                movement += axes[i].direction;
+     
+	}
+}
